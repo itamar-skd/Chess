bb2211e9f491b044cae20dc65bdbe6c266829f97
shaitamar <itamar.shaked1@gmail.com>
2026-02-15 19:22:35 +0000

    bishop implementation

--- src/chess_pieces/pieces/bishop_piece.cpp
+++ src/chess_pieces/pieces/bishop_piece.cpp
@@ -8,14 +8,33 @@ BishopPiece::BishopPiece(Position pos)
 
 bool BishopPiece::can_move_to(Position pos)
 {
-    return false; // TODO
+    if (pos.x >= CHESS_BOARD_SIZE || pos.y >= CHESS_BOARD_SIZE)
+        return false;
+
+    /**
+     * bishop moves diagonally. for linear functions with slope 1, by definition, for any two points (x1, y1) and (x2, y2), abs(x2 - x1) == abs(y2 - y1).
+     */
+    if (abs(pos.x - this->_pos.x) == abs(pos.y - this->_pos.y))
+        return true;
+
+    return false;
 }
 
 std::vector<Position> BishopPiece::all_possible_moves()
 {
     std::vector<Position> locations;
 
-    // TODO
+    for (int x = 0; x < CHESS_BOARD_SIZE; x++)
+    {
+        Position first_diagonal(x, this->_pos.y + (this->_pos.x - x));
+        Position second_diagonal(x, this->_pos.y - (this->_pos.x - x));
+
+        if (this->can_move_to(first_diagonal))
+            locations.push_back(first_diagonal);
+
+        if (this->can_move_to(second_diagonal))
+            locations.push_back(second_diagonal);
+    }
 
     return locations;
 }
